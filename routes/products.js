5e7ee4721819acaafc1db1014d3f16482ce2b2a8
Kevin Pleitez <kevin.pleitez@creativaconsultores.com>
2018-10-27 12:39:58 -0600

    id autoincrementet

--- routes/products.js
+++ routes/products.js
@@ -17,7 +17,27 @@ router.get('/getAll', (req, res) => {
 router.post('/save', (req, res) => {
     var bd = req.app.get("bd");
     var product =  req.body;
+    var last = bd.productos[bd.productos.length-1];
+    product["id"] = last.id + 1;
     bd.productos.push(product);
+    res.json({
+        success:true
+    });
+});
+
+router.post('/delete/:id', (req, res) => {
+    var bd = req.app.get("bd");
+    var id = req.params.id
+    var index = -1;
+    bd.productos.forEach((val,index,arr)=>{
+        if(val["id"] == id){
+            index = id;
+        }
+    });
+    if(index != -1){
+        bd.productos.splice(index, 1);
+    }
+
     res.json({
         success:true
     });
